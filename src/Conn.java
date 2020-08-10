ce2560afc7ba4e035acd1c7f875dc316a3521609
Shikha Rajpoot <64718956+shikhacodes@users.noreply.github.com>
2020-08-10 16:13:27 +0530

    Add files via upload

---
+++ src/Conn.java
@@ -0,0 +1,19 @@
+package university.management.system;
+
+import java.sql.*;
+
+public class Conn {
+    Connection c;
+    Statement s;
+    public Conn(){
+        try{
+        Class.forName("com.mysql.jdbc.Driver");
+        c = DriverManager.getConnection("jdbc:mysql:///ums","root","");
+        s = c.createStatement();
+        }catch(Exception e){
+        System.out.println(e);
+        }
+
+    }
+
+}
