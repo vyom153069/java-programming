1bfe2d665d2288eaa64717f1b9e351d8d156f4f6
Shikha Rajpoot <64718956+shikhacodes@users.noreply.github.com>
2020-08-12 12:45:22 +0530

    Update Splash.java

--- University Management System/src/university/management/system/Splash.java
+++ University Management System/src/university/management/system/Splash.java
@@ -1,19 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package university.management.system;
 
-/**
- *
- * @author Bhuvnesh Kumar
- */
 public class Splash extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Splash
-     */
     public Splash() {
         initComponents();
     }
