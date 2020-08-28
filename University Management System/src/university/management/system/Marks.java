438d6f9dc80caf7b1b74b9656e88766d0a6fe5b5
gsanya1003 <gsanya1003@gmail.com>
2020-08-28 12:23:54 +0530

    Marks uploaded

--- University Management System/src/university/management/system/Marks.java
+++ University Management System/src/university/management/system/Marks.java
@@ -1,14 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package university.management.system;
 
-/**
- *
- * @author Bhuvnesh Kumar
- */
-public class Marks {
-    
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.sql.*;
+
+public class Marks extends JFrame{
+
+   JTextArea t1;
+   JPanel p1;
+   
+   Marks(){}
+   Marks(String str){
+       setSize(500,600);
+       setLayout(new BorderLayout());
+       
+       p1 = new JPanel();
+
+       t1 = new JTextArea(50,15);
+       JScrollPane jsp = new JScrollPane(t1);
+       t1.setFont(new Font("sitka text",Font.BOLD,18));
+       t1.setForeground(new Color(15,19,42));
+       
+       add(p1,"North");
+       
+       add(jsp,"Center");
+       
+       setLocation(450,200);
+       mark(str);
+   }
+   
+   public void mark(String s){
+    try{
+           Conn c = new Conn();
+           
+           t1.setText("\tResult of Examination\n\n\tSubject\n");
+           
+           ResultSet rs1 = c.s.executeQuery("select * from subject where rollno="+s);
+           
+           if(rs1.next()){
+               t1.append("\n\t"+rs1.getString("subject1"));
+               t1.append("\n\t"+rs1.getString("subject2"));
+               t1.append("\n\t"+rs1.getString("subject3"));
+               t1.append("\n\t"+rs1.getString("subject4"));
+               t1.append("\n\t"+rs1.getString("subject5"));
+               t1.append("\n------------------------------------------------");
+               t1.append("\n");
+           }
+           
+           ResultSet rs2 = c.s.executeQuery("select * from marks where rollno="+s);
+           
+           if(rs2.next()){
+               t1.append("\n\tMarks\n\n\t"+rs2.getString("marks1"));
+               t1.append("\n\t"+rs2.getString("marks2"));
+               t1.append("\n\t"+rs2.getString("marks3"));
+               t1.append("\n\t"+rs2.getString("marks4"));
+               t1.append("\n\t"+rs2.getString("marks5"));
+               t1.append("\n------------------------------------------------");
+               t1.append("\n");
+           }
+         
+       }catch(Exception e){
+           e.printStackTrace();
+       }
+   }
+   
+   public static void main(String[] args){
+       new Marks().setVisible(true);
+   }
 }
